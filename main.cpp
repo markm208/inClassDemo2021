2559df41a0413cfa0a25744811b1a7ef590ee33c
Mark Mahoney <markm208@gmail.com>
2021-02-05 14:08:06 -0600

    Added a square function

--- main.cpp
+++ main.cpp
@@ -2,9 +2,22 @@
 
 using namespace std;
 
+void square(int num);
+
 int main()
 {
     cout<<"GitHub Test"<<endl;
     
+    square(1);
+    square(5);
+    square(10);
+    square(0);
+    square(-10);
+
     return 0;
+}
+//--
+void square(int num)
+{
+    cout<<(num * num)<<endl;
 }
